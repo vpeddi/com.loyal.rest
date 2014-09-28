86db346737ee3f816ccd49a747fa29e7c44f19ba
VENKATESHAM <venkat.peddi@hotmail.com>
2014-09-28 13:53:01 -0500

    adding restful webservice code

---
+++ src/com/loyal/rest/status/V1_status.java
@@ -0,0 +1,25 @@
+package com.loyal.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+@Path("/v1/status/")
+public class V1_status {
+
+	private static final String api_version = "00.01.00";
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle()
+	{
+		return "<p> Java Web Services </p>";
+	}
+
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion()
+	{
+		return "<p> Version:: </p>" + api_version;
+	}
+
+	
+}
